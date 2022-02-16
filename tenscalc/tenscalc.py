c74a4f56709d618af1c5cae134624d2e76855933
David E. Lambert <david@davidelambert.com>
2022-02-16 13:53:19 -0500

    MAJOR REORGANIZATION
    rework using unit weights directly instead of coefficients
    
    collect unit weights from CSV charts into a single JSON
    
    move data for main & test functions to tenscalc/data/

--- tenscalc/tenscalc.py
+++ tenscalc/tenscalc.py
@@ -1,19 +1,19 @@
 import json
 from pathlib import Path
 
-PROJECT_ROOT = Path(__file__).parent.parent.parent
+PROJECT_ROOT = Path(__file__).parent.parent
 
-with open('./tension_coefs.json', 'r') as f:
-    coef = json.load(f)
+with open(PROJECT_ROOT/'tenscalc/data/unit_weights.json', 'r') as f:
+    data = json.load(f)
 
-with open('./frequency_chart.json', 'r') as f:
+with open(PROJECT_ROOT/'tenscalc/data/frequency_chart.json', 'r') as f:
     freq = json.load(f)
 
 
 def get_uw(gauge, material):
     if gauge >= 1.0:
         gauge = gauge / 1000
-    uw = coef[material] * (gauge**2)
+    uw = data[material]['data'][str(gauge)]
     return uw
 
 
