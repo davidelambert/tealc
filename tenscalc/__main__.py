51ee3fa0310f9f5cbd9a06b66f75a8ddced4faa2
David E. Lambert <david@davidelambert.com>
2022-02-18 20:03:09 -0500

    adds non-mandatory positional args
    material, pitch, and length may now be specified either as positional args (tenscalc .011 nps e4 [25.5]) OR with argument tags (tenscalc .011 -m nps -p e4 [-l 25.5])

--- tenscalc/__main__.py
+++ tenscalc/__main__.py
@@ -2,15 +2,23 @@ import argparse
 from . import common
 
 parser = argparse.ArgumentParser(prog='tenscalc',
-                                 description='Stringed instrument tension calculator')
-parser.add_argument('-g', '--gauge', type=float)
-parser.add_argument('-m', '--material',
-                    choices=['ps', 'nps', 'pb', '8020', '8515', 'ss', 'pn', 'fw'])
+        description='Stringed instrument tension calculator',
+        usage="""tenscalc [-h] GAUGE MATERIAL PITCH [length]
+   OR: tenscalc [-h] GAUGE -m MATERIAL -p PITCH [-l LENGTH]""")
+
+parser.add_argument('gauge', type=float)
+parser.add_argument('-m', '--material', metavar='MATERIAL',
+        choices=['ps', 'nps', 'pb', '8020', '8515', 'ss', 'pn', 'fw'])
+parser.add_argument('material', nargs='?', metavar='material',
+        choices=['ps', 'nps', 'pb', '8020', '8515', 'ss', 'pn', 'fw'])
 parser.add_argument('-p', '--pitch')
-parser.add_argument('-s', '--scale_length', type=float, default=25.5)
+parser.add_argument('pitch', nargs='?')
+parser.add_argument('-l', '--length', type=float, default=25.5)
+parser.add_argument('length', nargs='?', type=float, default=25.5)
 
 args = parser.parse_args()
 
 uw = common.get_uw(args.gauge, args.material)
-lbs = common.tension(uw, args.pitch, args.scale_length)
+lbs = common.tension(uw, args.pitch, args.length)
 print('{:.2f} lbs'.format(lbs))
+
