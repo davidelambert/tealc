00ab2f616895317d4010b2fcd9775655af574606
David E. Lambert <david@davidelambert.com>
2022-02-21 17:06:05 -0500

    add support for SI units

--- tenscalc/__main__.py
+++ tenscalc/__main__.py
@@ -7,9 +7,18 @@ parser.add_argument('gauge', type=float)
 parser.add_argument('material')
 parser.add_argument('pitch')
 parser.add_argument('length', nargs='?', type=float, default=25.5)
+parser.add_argument('--si', action='store_true')
 
 args = parser.parse_args()
 
+if args.si:
+    args.gauge = round(args.gauge / 25.4, 3)
+    args.length = args.length / 25.4
+
 uw = common.get_uw(args.gauge, args.material)
 lbs = common.tension(uw, args.pitch, args.length)
-print('{:.2f} lbs'.format(lbs))
+
+if args.si:
+    print('{:.2f} kg'.format(lbs / 2.205))
+else:
+    print('{:.2f} lbs'.format(lbs))
