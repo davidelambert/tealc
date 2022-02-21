2318594842fed462f4a39a93c66ce53ba2b55c37
David E. Lambert <david@davidelambert.com>
2022-02-21 16:52:05 -0500

    positional args only
    Removes syntax using optional args for material, pitch, and length.
    
    Having two different syntaxes, one using positional args and the other using options/flags, could get very confusing and hard to support. Use positional argument syntax only.

--- tenscalc/__main__.py
+++ tenscalc/__main__.py
@@ -1,21 +1,12 @@
 import argparse
 from . import common
 
-parser = argparse.ArgumentParser(prog='tenscalc',
-                                 usage="""tenscalc [-h] GAUGE MATERIAL PITCH [LENGTH]
-   OR: tenscalc [-h] GAUGE -m MATERIAL -p PITCH [-l LENGTH]""")
+parser = argparse.ArgumentParser(prog='tenscalc')
 
-parser.add_argument('gauge', type=float, metavar='GAUGE')
-parser.add_argument('-m', '--material', metavar='MATERIAL',
-                    choices=['ps', 'nps', 'pb', '8020', '8515', 'ss', 'pn', 'fw'])
-parser.add_argument('material', nargs='?', metavar='MATERIAL',
-                    choices=['ps', 'nps', 'pb', '8020', '8515', 'ss', 'pn', 'fw'])
-parser.add_argument('-p', '--pitch', metavar='PITCH')
-parser.add_argument('pitch', nargs='?', metavar='PITCH')
-parser.add_argument('-l', '--length', metavar='LENGTH',
-                    type=float, default=25.5)
-parser.add_argument('length', nargs='?', metavar='LENGTH',
-                    type=float, default=25.5)
+parser.add_argument('gauge', type=float)
+parser.add_argument('material')
+parser.add_argument('pitch')
+parser.add_argument('length', nargs='?', type=float, default=25.5)
 
 args = parser.parse_args()
 
