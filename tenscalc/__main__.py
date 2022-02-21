f984d9ff0f656e448558536f3399291876dd35d8
David E. Lambert <david@davidelambert.com>
2022-02-21 17:53:02 -0500

    add validation for material arg

--- tenscalc/__main__.py
+++ tenscalc/__main__.py
@@ -1,4 +1,7 @@
 import argparse
+import sys
+import json
+import re
 from .common import get_uw, tension
 
 parser = argparse.ArgumentParser(prog='tenscalc')
@@ -10,6 +13,18 @@ parser.add_argument('length', nargs='?', type=float, default=25.5)
 parser.add_argument('--si', action='store_true')
 
 args = parser.parse_args()
+
+# MATERIAL CODE VALIDATION ================================
+with open('./tenscalc/data/material_codes.json', 'r') as f:
+    material_codes = json.load(f)
+
+if args.material not in list(material_codes):
+    print('\nERROR: material must be one of the following codes')
+    print('Code'.rjust(6), 'String material', sep='  ')
+    print('----'.rjust(6), '---------------', sep='  ')
+    for k, v in material_codes.items():
+        print(k.rjust(6), v, sep='  ')
+    sys.exit()
 
 if args.si:
     args.gauge = round(args.gauge / 25.4, 3)
