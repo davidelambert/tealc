69ef6b6be348b66814e0b91bce7cb07708650984
David E. Lambert <david@davidelambert.com>
2022-02-22 13:10:14 -0500

    move validation to common

--- tenscalc/__main__.py
+++ tenscalc/__main__.py
@@ -1,6 +1,4 @@
 import argparse
-import sys
-import json
 from .common import tension
 
 parser = argparse.ArgumentParser(prog='tenscalc')
@@ -13,27 +11,6 @@ parser.add_argument('--si', action='store_true')
 
 args = parser.parse_args()
 
-# MATERIAL CODE VALIDATION ================================
-with open('./tenscalc/data/material_codes.json', 'r') as f:
-    material_codes = json.load(f)
-
-if args.material not in list(material_codes):
-    print('\nERROR: material must be one of the following codes')
-    print('Code'.rjust(6), 'String material', sep='  ')
-    print('----'.rjust(6), '---------------', sep='  ')
-    for k, v in material_codes.items():
-        print(k.rjust(6), v, sep='  ')
-    sys.exit()
-
-# PITCH VALIDATION =======================================
-with open('./tenscalc/data/frequency_chart.json', 'r') as f:
-    frequency_chart = json.load(f)
-
-if args.pitch not in list(frequency_chart):
-    print('\n ERROR: pitch must be in scientific pitch notation, from A0-E5,')
-    print('  with uppercase note letter.')
-    print('See help [-h, --help] for examples and common open string pitches')
-    sys.exit()
 
 # SI CONVERSION ==========================================
 if args.si:
--- tenscalc/common.py
+++ tenscalc/common.py
@@ -1,5 +1,6 @@
 import json
 from pathlib import Path
+import sys
 
 PROJECT_ROOT = Path(__file__).parent.parent
 
@@ -11,6 +12,31 @@ with open(PROJECT_ROOT/'tenscalc/data/frequency_chart.json', 'r') as f:
 
 with open(PROJECT_ROOT/'tenscalc/data/tension_coefs.json', 'r') as f:
     coefs = json.load(f)
+
+with open(PROJECT_ROOT/'tenscalc/data/material_codes.json', 'r') as f:
+    mat_codes = json.load(f)
+
+
+def validate_material(material):
+    just_chars = 11
+    if material not in list(mat_codes):
+        print('\nERROR: material must be one of the following codes:')
+        print('Code'.rjust(just_chars), 'String material', sep='  ')
+        print('----'.rjust(just_chars), '---------------', sep='  ')
+        for k, v in mat_codes.items():
+            print(k.rjust(just_chars), v, sep='  ')
+        sys.exit()
+    else:
+        pass
+
+
+def validate_pitch(pitch):
+    if pitch not in list(freq):
+        print('\nERROR: pitch must be in scientific pitch notation,')
+        print('       from A0-E5, with uppercase note letter.')
+        sys.exit()
+    else:
+        pass
 
 
 def get_uw(gauge, material):
@@ -28,6 +54,9 @@ def get_uw(gauge, material):
 def tension(gauge: float, material: str, pitch: str, scale_length: float):
     if gauge >= 1.0:
         gauge = gauge / 1000
+
+    validate_material(material)
+    validate_pitch(pitch)
 
     unit_weight = get_uw(gauge, material)
     hz = freq[pitch]
