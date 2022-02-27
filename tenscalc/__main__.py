0530237585b07e04c9cf083c71ec600d5be0916b
David E. Lambert <david@davidelambert.com>
2022-02-27 15:38:52 -0500

    introduce subcommands
    Principal single-string functionality is now `tenscalc string [args]`,
    with short usage/help available with `tenscalc string -h`.
    
    `tenscalc -h` now shows short usage/help, while new `tenscalc help`
    syntax displays the fuller manual or "long help".
    
    Manual is now read from an external plain text file for modularity.
    
    This approach is cleaner and will allow for the addition of a `tenscalc
    set` command to supply an entire string set via file for command line.

--- tenscalc/__main__.py
+++ tenscalc/__main__.py
@@ -1,54 +1,37 @@
 import argparse
-import sys
-from .common import tension
+from common import tension
 
-helpstring = '''usage: tenscalc <gauge> <material> <pitch> <length> [--si]
-       tenscalc --help
+with open('./tenscalc/data/manual.txt', 'r') as f:
+    manual = f.read()
 
-Required arguments:
-  gauge     String gauge in inches, or in mm with the --si flag.
-            Gauges in inches may be specified in thousandths of an inch:
-            "11" or ".011" are both valid
-  material  Code for string construction material. Current options are:
-              Code  Material
-              ----  --------
-                ps  Plain Steel
-               nps  Nickel-Plated Steel Wound
-                pb  Phosphor Bronze Wound
-              8020  80/20 Bronze Wound
-              8515  85/15 Bronze Wound
-                ss  Stainless Steel Roundwound
-                fw  Stainless Steel Flatwound
-                pn  Pure Nickel Wound
-  pitch     Tuned pitch of string in scientific pitch notation, from A0-E5.
-            Examples of open-string pitches in standard tunings:
-                Guitar: E2, A2, D3, G3, B3, E4
-                  Bass: (B0), E1, A1, D2, G2
-              Mandolin: G3, D4, A4, E5
-                 Banjo: G4, D3, G3, B3, D4
-  length    Scale length of instrument in inches, or in mm with the --si flag.
+parser = argparse.ArgumentParser(prog='tenscalc')
+subparsers = parser.add_subparsers(dest='command')
 
-Optional arguments:
-  --si      <gauge> and <length> are interpreted as mm; tension is returned in kg.
-  --help    Show this help message and exit
+string_parser = subparsers.add_parser('string',
+                                      help='calculate tension for a single string',
+                                      usage='tenscalc string [-h] [--si] <gauge> <material> <pitch> <length>')
+string_parser.add_argument('gauge', type=float,
+                           help='inches, 1/1000 of an inch, or mm with --si flag')
+string_parser.add_argument('material', metavar='material',
+                           choices=['ps', 'nps', 'pb', '8020', '8515', 'ss', 'fw', 'pn'],
+                           help='options: ps, nps, pb, 8020, 8515, ss, fw, pn')
+string_parser.add_argument('pitch',
+                           help='e.g. A1, Bb2, C#3. C4 is middle C')
+string_parser.add_argument('length', type=float,
+                           help='scale length in inches, or mm with --si flag')
+string_parser.add_argument('--si', action='store_true',
+                           help='<gauge> and <length> in mm; return tension in kg')
 
-Examples:
-  tenscalc .011 ps E4 25.5
-  tenscalc --si 1.37 pb E2 632.5'''
+# TODO: set syntax
+# set_parser = subparsers.add_parser('set', help='calculate tension for a set of strings')
+# set_parser.add_argument('file', help='see `tenscalc help` for formatting')
 
-parser = argparse.ArgumentParser(prog='tenscalc', add_help=False)
-parser.add_argument('gauge', nargs='?', type=float)
-parser.add_argument('material', nargs='?')
-parser.add_argument('pitch', nargs='?')
-parser.add_argument('length', nargs='?', type=float)
-parser.add_argument('--si', action='store_true')
-parser.add_argument('--help', action='store_true')
+help_parser = subparsers.add_parser('help', help='print long help', add_help=False)
+
 args = parser.parse_args()
 
-if args.help:
-    print(helpstring)
-    sys.exit()
-else:
+
+if args.command == 'string':
     value = tension(args.gauge, args.material, args.pitch,
                     args.length, args.si)
     if args.si:
@@ -56,3 +39,13 @@ else:
     else:
         units = 'lbs'
     print('{:.2f} {}'.format(value, units))
+
+# TODO: set logic
+# elif args.command == 'set':
+#     pass
+
+elif args.command == 'help':
+    print(manual)
+
+else:
+    parser.print_help()
