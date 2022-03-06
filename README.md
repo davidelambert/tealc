d112878ab92b9ca40014a9d6823cc679397e8a46
David E. Lambert <david@davidelambert.com>
2022-03-06 17:57:37 -0500

    MIT => GPLv3

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 ## tenscalc
 
-tenscalc is a stringed instrument tension calculator with arguments for string gauge, material, pitch, and instrument scale length. Tension estimates for various string materials are based on published tension guides from a common US-based string manufacturer. 
+tenscalc is a stringed instrument tension calculator with arguments for
+string gauge, material, pitch, and instrument scale length. Tension
+estimates for various string materials are based on published tension
+guides from a common US-based string manufacturer. 
--- tenscalc/__init__.py
+++ tenscalc/__init__.py
@@ -1,3 +1,15 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2022 David E. Lambert
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 """A stringed instrument tension calculator.
 
 Modules:
@@ -10,3 +22,7 @@ from tenscalc.tenscalc import (StringTension,
                                SetFileParser,
                                print_material_codes,
                                manual)
+
+__version__ = '0.1.0'
+__author__ = 'David E. Lambert'
+
