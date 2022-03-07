0301d7735d34156d31e4933a1d7ead50372ebbcf
David E. Lambert <david@davidelambert.com>
2022-03-07 10:49:37 -0500

    CLI entry point
    Squashes a few commits that convert the CLI code formerly under
    __main__.py to a separate cli.py module that setuptools installs as an
    entry point with the console command "tenscalc"
    
    commit 0e7568668c5c0da7c17d0e334cc088d981617e0b
    Author: David E. Lambert <david@davidelambert.com>
    Date:   Sun Mar 6 21:59:09 2022 -0500
    
        tweak cli.py for testpy
    
    commit 784cd3f30fc2a02027eb6725e0dfec65eefbfbaf
    Author: David E. Lambert <david@davidelambert.com>
    Date:   Sun Mar 6 21:57:20 2022 -0500
    
        remove manual; add print_manual()
    
    commit 4f2fbd017ceaa450809d1797ba380c74c7224a82
    Author: David E. Lambert <david@davidelambert.com>
    Date:   Sun Mar 6 18:42:15 2022 -0500
    
        add cli.py entry point
    
        Converts old __main__.py to cli.py.
        Wraps CLI code in main() function and adds execution guard.
        Rewrites __main__.py to import tenscalc.cli.main and call it.
        Adds cli:main as an entry point for setuptools
        Corrects a test file error and adds pytest config to pyproject.toml.

--- tenscalc/__init__.py
+++ tenscalc/__init__.py
@@ -1,15 +1,16 @@
 # -*- coding: utf-8 -*-
 # Copyright (C) 2022 David E. Lambert
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
+
 """A stringed instrument tension calculator.
 
 Modules:
@@ -20,9 +21,7 @@ Modules:
 from tenscalc.tenscalc import (StringTension,
                                StringSet,
                                SetFileParser,
-                               print_material_codes,
-                               manual)
+                               print_material_codes)
+from tenscalc.cli import main, print_manual
 
 __version__ = '0.1.0'
-__author__ = 'David E. Lambert'
-
---
+++ tenscalc/cli.py
@@ -0,0 +1,124 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2022 David E. Lambert
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+"""Command line interface for tenscalc.
+
+Code in this module is run when tenscalc is tun from the command line.
+    This package contains no public objects. Documentation for the CLI
+    is contained externally in tenscalc.manual, which is accessible
+    via the command line with ``tenscalc help``.
+"""
+
+import os
+import subprocess
+import argparse
+
+from tenscalc import StringTension, StringSet, SetFileParser
+
+msg = {
+    'string': 'calculate tension for a single string',
+    'set': 'calculate tensions for a set of strings',
+    'gauge': 'inches, 1/1000 of an inch, or mm with --si flag',
+    'mat': 'options: ps, nps, pb, 8020, 8515, ss, fw, pn',
+    'pitch': 'e.g. A1, Bb2, C#3. C4 is middle C',
+    'length': 'scale length in inches, or mm with --si flag',
+    'si': 'supply gauge and length units in mm; get tension in kg',
+    'title': 'optional title for output chart',
+    'double': 'double each string spec (for instruments like mandolin)',
+    'file': 'see tenscalc -h for format'
+}
+
+set_usage = """tenscalc set [-h] [--file FILE] [--title TITLE]
+    tenscalc set [-h] [--length LENGTH] [--gauges [G ...]]
+                        [--materials [M ...]] [--pitches [P ...]]
+                        [--double] [--si] [--title TITLE]"""
+
+parser = argparse.ArgumentParser(prog='tenscalc')
+subparsers = parser.add_subparsers(dest='command')
+
+string_parser = subparsers.add_parser('string', help=msg['string'])
+string_parser.add_argument('gauge', type=float, help=msg['gauge'])
+string_parser.add_argument('material', metavar='material', help=msg['mat'],
+                           choices=['ps', 'nps', 'pb', '8020',
+                                    '8515', 'ss', 'fw', 'pn'])
+string_parser.add_argument('pitch', help=msg['pitch'])
+string_parser.add_argument('length', type=float, help=msg['length'])
+string_parser.add_argument('--si', action='store_true', help=msg['si'])
+
+set_parser = subparsers.add_parser('set', help=msg['set'], usage=set_usage)
+set_parser.add_argument('--file', help=msg['file'])
+set_parser.add_argument('--length', type=float, help=msg['length'])
+set_parser.add_argument('--gauges', nargs='*', metavar='G', help=msg['gauge'])
+set_parser.add_argument('--materials', nargs='*', metavar='M', help=msg['mat'])
+set_parser.add_argument('--pitches', nargs='*', metavar='P', help=msg['pitch'])
+set_parser.add_argument('--double', help=msg['double'], action='store_true')
+set_parser.add_argument('--si', help=msg['si'], action='store_true')
+set_parser.add_argument('--title', help=msg['title'])
+
+help_parser = subparsers.add_parser('help', help='print manual',
+                                    add_help=False)
+
+
+def print_manual():
+    """Print the manual with less (or more on Windows)."""
+    if os.name == 'posix':
+        prompt = '-Pstenscalc help line %lt/%L (press h for help or q to quit)'
+        subprocess.run(['less', prompt, '--', './tenscalc/data/manual.txt'])
+    elif os.name == 'nt':
+        subprocess.run(['more', './tenscalc/data/manual.txt'])
+    else:
+        print('./tenscalc/data/manual.txt')
+
+
+def main(args: list[str] = None):
+    """Command line interface for tenscalc.
+
+    Args:
+        args (list[str], optional): Argument list. The default of ``None``
+        will read sys.argv
+    """
+    parsed_args = parser.parse_args(args)
+
+    if parsed_args.command == 'string':
+        tension = StringTension(parsed_args.gauge, parsed_args.material,
+                                parsed_args.pitch, parsed_args.length,
+                                parsed_args.si)
+        if parsed_args.si:
+            print('{:.1f} kg'.format(tension.kg))
+        else:
+            print('{:.1f} lb'.format(tension.lb))
+
+    elif parsed_args.command == 'set':
+        if parsed_args.file is None:
+            tension = StringSet(parsed_args.length, parsed_args.gauges,
+                                parsed_args.materials, parsed_args.pitches,
+                                parsed_args.double, parsed_args.si)
+        else:
+            sf = SetFileParser(parsed_args.file)
+            tension = StringSet(sf.length, sf.gauges, sf.materials, sf.pitches,
+                                sf.double, sf.si)
+
+        if parsed_args.si:
+            tension.print(parsed_args.title, print_si=True)
+        else:
+            tension.print(parsed_args.title)
+
+    elif parsed_args.command == 'help':
+        print_manual()
+
+    else:
+        parser.print_help()
+
+
+if __name__ == '__main__':
+    main()
