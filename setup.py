e8a2afd4abb3f29acf1cebe45bd20bc4a8b2829f
David E. Lambert <david@davidelambert.com>
2022-03-21 15:22:48 -0400

    0.2.0a1 versioning

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = (here/'README.md').read_text(encoding='utf-8')
 
 setup(
     name='tealc',
-    version='0.2.0',
+    version='0.2.0a1',
     author='David E. Lambert',
     author_email='david@davidelambert.com',
     description='Tension estimator for stringed instruments.',
