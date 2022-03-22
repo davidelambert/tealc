e2f7a0c00b069e65f154ed1f3189120247521f1c
David E. Lambert <david@davidelambert.com>
2022-03-22 19:04:00 -0400

    rework SetFileParser
    Change the format of setfiles to be more intuitive, with a single
    "length = ..." line, one "GAUGE MATERIAL PITCH" line per string, and an
    optional "si = True (or False)" line. This format matches the GAUGE
    MATERIAL PITCH format of the "tealc set" CLI command.
    
    Changing the format eliminates configparser import while adding the
    new re import (so no external dependencies).
    
    This commit does NOT adjust cli.py or manual.txt to reflect the updates.

--- tealc/tealc.py
+++ tealc/tealc.py
@@ -54,8 +54,7 @@ Attributes:
 
 from pathlib import Path
 import json
-import configparser
-import copy
+import re
 
 PKG_DIR = Path(__file__).parent
 
@@ -211,10 +210,7 @@ class StringSet:
                  materials: list, pitches: list,
                  si=False):
         """Class constructor."""
-        try:
-            self.si = bool(si)
-        except Exception:
-            raise ValueError(_err_msg['si'])
+        self.si = si
 
         try:
             assert type(length) in (float, int)
@@ -311,92 +307,105 @@ class StringSet:
 
 
 class SetFileParser:
-    """Parse arguments for a StringSet object contained in a text file.
+    """Parse a formatted set file for arguments passable to StringSet().
 
-    Set files follow configparser conventions and use the format:
+    A set file has the following format, where properties are as
+    described in the Attributes section below.
+        length = LENGTH
+        GAUGE MATERIAL PITCH
+        [GAUGE MATERIAL PITCH]
+        [...]
+        [si = True or False]
 
-    ---- begin set file ---
-    [set]
-    length = float
-    gauges = float [float ...]
-    materials = str [str ...]
-    pitches = str [str ...]
-    si = bool (optional)
-    ---- end set file ----
+    Thus, a common set of "10 gauge" electric guitar strings, in
+    standard guitar tuning, on a Fender-scaled instrument could be
+    described in a set file like this:
+        length = 25.5
+        10 ps e6
+        13 ps b3
+        17 ps g3
+        26 nps d3
+        36 nps a2
+        46 nps e2
 
-    The [set] section header and all keys are required. Any other
-        sections or keys are ignored. Lists for gauges, materials, and
-        pitches keys must be of equal length. List items are
-        space-separated.
+    The "length = ..." line and at least one "GAUGE MATERIAL PITCH"
+    line are required.
+
+    "si = False" is not required, and the SetFileParser.si attribute
+    defaults to False if no "si = ..." line is included.
 
     Attributes:
-        length (float): Parsed from input file.
-        gauges (list[float]): Parsed from input file.
-        materials (list[str]): Parsed from input file.
-        pitches (list[str]): Parsed from input file.
-        si (bool): : Parsed from input file.
+        length (float): Instrument scale length in inches, 1/1000in,
+            or mm with si=True.
+        gauges (list[float]): List of string gauges in inches,
+            1/1000in, or mm with si=True.
+        materials (list[str]): List of valid string material codes.
+        pitches (list[str]): List of pitches in scientific pitch
+            notation, from A0-E5.
+        si (bool, optional): Supply length and gauges in mm.
+            Defaults to False.
+        lines (list[str]): List of lines in input file.
 
     Args:
-        file (path-like): A test file using the above format.
+        file (path-like): Path to a properly formatted set file.
 
     Raises:
-        TypeError: on non-numeric length or gauges.
-        KeyError: on invalid material codes or pitches.
-        ValueError: on non-boolean si.
-        AssertionError: on gauges, materials, and pitches lists
-            of differing length.
+        KeyError: On invalid material codes or pitches.
+        ValueError: On missing length or string properties
     """
-
-    _required_keys = ['length', 'gauges', 'materials', 'pitches']
 
     def __init__(self, file):
         """Class constructor."""
-        p = configparser.ConfigParser()
-        p.read(file)
+        self.length = None
+        self.gauges = []
+        self.materials = []
+        self.pitches = []
+        self.si = False
 
-        if not p.has_section('set'):
-            raise KeyError('file must include the heading "[set]"')
+        with open(file, 'r') as f:
+            self.lines = [ln.replace('\n', '') for ln in f.readlines()]
 
-        for k in self._required_keys:
-            if k not in p['set'].keys():
-                raise KeyError('missing required key {}'.format(k))
+        l_re = re.compile(r'length\s*=\s*(\d+\.*\d*)')
+        s_re = re.compile(r'(\d*\.\d+|\d+\.*\d*)\s*(\w+)\s*([a-gA-G][b#]?\d+)')
+        si_re = re.compile(r'si\s*=\s*(True|true|1|False|false|0)')
 
-        try:
-            self.length = float(p['set']['length'])
-        except Exception:
-            raise ValueError(_err_msg['length'])
+        for line in self.lines:
+            l_match = l_re.match(line)
+            if l_match is not None:
+                self.length = float(l_match.groups()[0])
 
-        try:
-            self.gauges = list(map(float, (p['set']['gauges']
-                                           .replace(', ', '')
-                                           .split())
-                                   ))
-        except Exception:
-            raise ValueError(_err_msg['gauge'])
+            s_match = s_re.match(line)
+            if s_match is not None:
+                gauge, material, pitch = s_match.groups()
 
-        try:
-            self.materials = p['set']['materials'].replace(', ', '').split()
-            for mat in self.materials:
-                assert mat in material_codes.keys()
-        except Exception:
-            raise ValueError(_err_msg['mat'])
+                self.gauges.append(float(gauge))
 
-        try:
-            self.pitches = p['set']['pitches'].split()
-            for pitch in self.pitches:
-                assert pitch[0].upper() + pitch[1:] in frequency_chart.keys()
-        except Exception:
-            raise ValueError(_err_msg['pitch'])
+                try:
+                    assert material in material_codes.keys()
+                except Exception:
+                    raise KeyError(_err_msg['mat'])
+                finally:
+                    self.materials.append(material)
 
-        try:
-            assert len(self.gauges) == len(self.materials) == len(self.pitches)
-        except Exception:
-            raise AssertionError(_err_msg['arglen'])
+                pitch = pitch[0].upper() + pitch[1:]
+                try:
+                    assert pitch in frequency_chart.keys()
+                except Exception:
+                    raise KeyError(_err_msg['pitch'])
+                finally:
+                    self.pitches.append(pitch)
 
-        if 'si' in p['set'].keys():
-            try:
-                self.si = p.getboolean('set', 'si')
-            except Exception:
-                raise ValueError(_err_msg['si'])
-        else:
-            self.si = False
+            si_match = si_re.match(line)
+            if si_match is not None:
+                if si_match.groups()[0] in ('True', 'true', '1'):
+                    self.si = True  # already False, so no "else:"
+
+        if self.length is None:
+            msg = 'File must include "length = (number)"'
+            raise ValueError(msg)
+
+        if (len(self.gauges) == 0
+                or len(self.materials) == 0
+                or len(self.pitches) == 0):
+            msg = 'File must include at least one GAUGE MATERIAL PITCH line'
+            raise ValueError(msg)
